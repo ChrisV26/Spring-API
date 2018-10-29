5385663a33b204abfa1b335c91bb80762ff0f7a8
Chris Vera <chrissmith132@gmail.com>
2018-10-29 21:29:31 +0200

    Added new JSON response in controller

--- src/main/java/WebService/ApplicationController.java
+++ src/main/java/WebService/ApplicationController.java
@@ -2,6 +2,7 @@ package WebService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +17,17 @@ public class ApplicationController {
 
    /* With these methods we handle the queries for the db */
 
-    // Return all rows from tour_app db
+    // Return all rows from poi table
    @GetMapping(path="/poi")
     public List<POI> index()
    {
        return tourAppRepository.findAll();
    }
+
+   // Return all rows of lat,lng where Tour=1 from poi table
+    @GetMapping(path="poi/{Tour}")
+    public List<POI> showTour(@PathVariable int Tour){
+       return tourAppRepository.findByTourChoice(Tour);
+    }
+
 }
--- src/main/java/WebService/TourAppRepository.java
+++ src/main/java/WebService/TourAppRepository.java
@@ -1,10 +1,18 @@
 package WebService;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository  // This class talks to to the database adn is referred as Repository
 public interface TourAppRepository extends JpaRepository<POI,Integer> {
 
     //custom query to db excluding the common ones
+
+
+    @Query("SELECT lat,lng FROM POI poi WHERE Tour=?1")
+    List<POI> findByTourChoice(int Tour);
 }
